a3ce0a2e7343675f557b8d21c8f332007313e8da
SspStark <ssptonystark005@gmail.com>
2023-04-12 05:37:50 +0000

    add days

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const addDays = require("date-fns/addDays");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  const date = addDays(new Date(), 100);
+  response.send(
+    `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+  );
+});
+
+app.listen(3000);
